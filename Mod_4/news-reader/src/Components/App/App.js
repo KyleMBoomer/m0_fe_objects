13c40b1bf93003a6926db82285f801fb3cbcbb7a
“Kyle <kylemboomer@gmail.com>
2024-08-16 16:59:53 -0500

    Create apicalls.js

--- Mod_4/news-reader/src/Components/App/App.js
+++ Mod_4/news-reader/src/Components/App/App.js
@@ -1,5 +1,5 @@
-
-import 'src/App/App.css'
+import { useState, useEffect } from "react"
+import './App.css'
 
 function App() {
   return (
---
+++ Mod_4/news-reader/src/apicalls.js
@@ -0,0 +1,2 @@
+const api = process.env.REACT_APP_API_KEY
+let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${api}`
